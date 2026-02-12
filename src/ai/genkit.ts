759aa22aafe12e3cafee3efc0737478f3b09be8f
Robert Pellegrin <robert@altitudetrafficsolutions.com>
2026-02-12 16:15:29 +0000

    c

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
+    googleAI({ apiKey: "AIzaSyBA-NXbq_d7oWnQHsTWHJ_NtMvl6mkthdg" }),
   ],
   model: 'googleai/gemini-2.5-flash',
 });
